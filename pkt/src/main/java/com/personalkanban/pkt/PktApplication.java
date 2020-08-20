16ab36bc32ff525f43fb11424866571604c18203
arjun-sudo <laserarjun@gmail.com>
2020-08-20 19:45:02 +0545

    first commit

---
+++ pkt/src/main/java/com/personalkanban/pkt/PktApplication.java
@@ -0,0 +1,13 @@
+package com.personalkanban.pkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PktApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PktApplication.class, args);
+    }
+
+}
---
+++ pkt/src/test/java/com/personalkanban/pkt/PktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalkanban.pkt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PktApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
