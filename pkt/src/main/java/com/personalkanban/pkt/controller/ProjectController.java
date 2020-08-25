8d082b0ab3b3651846fc4e0e4bd3a1786ec6361b
arjun-sudo <laserarjun@gmail.com>
2020-08-25 17:11:02 +0545

    find project by id

--- pkt/src/main/java/com/personalkanban/pkt/controller/ProjectController.java
+++ pkt/src/main/java/com/personalkanban/pkt/controller/ProjectController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -38,4 +35,11 @@ public class ProjectController {
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 
     }
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId)
+    {
+        Project project=projectService.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project,HttpStatus.OK);
+    }
+
 }
--- pkt/src/main/java/com/personalkanban/pkt/service/ProjectService.java
+++ pkt/src/main/java/com/personalkanban/pkt/service/ProjectService.java
@@ -21,4 +21,16 @@ public class ProjectService {
 
         }
     }
+
+    public Project findProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project Id '" + projectId + "'does not exist");
+
+        } else {
+            return project;
+        }
+    }
+
 }
