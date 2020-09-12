af9e9177cf57c9d433b3d7c7173f3b1aeb427c20
arjun-sudo <laserarjun@gmail.com>
2020-09-12 21:16:59 +0545

    redux strore added

--- lkt-client/src/App.js
+++ lkt-client/src/App.js
@@ -5,15 +5,19 @@ import Header from "./components/Layout/Header";
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import AddProject from "./components/Project/AddProject";
 import LoginForm from "./components/Project/LoginForm";
+import {Provider} from "react-redux"
+import store from "./store";
 function App() {
   return (
       <Router>
+          <Provider store={store}>
           <div className="App">
               <Header/>
               <Route exact path={"/dashboard"} component={Dashboard}/>
               <Route exact path={"/addProject"} component={AddProject}/>
               <Route exact path={"/login"} component={LoginForm}/>
           </div>
+          </Provider>
       </Router>
 
   );
